c02a9d40631a5a2bae454aa0d4b9a8aca6321499
Derick Nguyen <100515167+Dericknlaser@users.noreply.github.com>
2025-10-30 01:11:54 -0500

    fixed previous commit

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Abilities/Ability")]
+public class Ability : ScriptableObject
+{
+    [Header("Basic Info")]
+    public string abilityName = "New Ability";
+    [TextArea(3, 5)]
+    public string description = "Ability description";
+    public Sprite icon;
+    
+    [Header("Stats")]
+    public int damage = 0;
+    public int healAmount = 0;
+    public int cooldownTurns = 0;
+    
+    [Header("Targeting")]
+    public AbilityTargetType targetType = AbilityTargetType.Self;
+    
+    [Header("Effects")]
+    public AbilityEffectType effectType = AbilityEffectType.Damage;
+	// Duration removed; effects should be handled externally if needed
+    
+    // [Header("Animation")]
+    // public string animationTrigger = "";
+    // public float animationDuration = 1f;
+    
+    // Method to execute the ability
+    public void Execute(Creature caster, Creature target = null)
+    {
+        switch (effectType)
+        {
+            case AbilityEffectType.Damage:
+                if (target != null && damage > 0)
+                {
+                    target.TakeDamage(damage);
+                    Debug.Log(caster.gameObject.name + " deals " + damage + " damage to " + target.gameObject.name + "!");
+                }
+                break;
+                
+            case AbilityEffectType.Heal:
+                if (target != null && healAmount > 0)
+                {
+                    target.Heal(healAmount);
+                    Debug.Log(caster.gameObject.name + " heals " + target.gameObject.name + " for " + healAmount + " HP!");
+                }
+                break;
+                
+            case AbilityEffectType.Defend:
+				// Increase defense; amount/duration handled elsewhere
+				Debug.Log(caster.gameObject.name + " defends!");
+                break;
+                
+            case AbilityEffectType.Buff:
+                // Apply buff for duration turns
+                Debug.Log(caster.gameObject.name + " applies buff to " + (target != null ? target.gameObject.name : "self") + "!");
+                break;
+                
+            case AbilityEffectType.Debuff:
+                // Apply debuff for duration turns
+                Debug.Log(caster.gameObject.name + " applies debuff to " + (target != null ? target.gameObject.name : "self") + "!");
+                break;
+        }
+    }
+    
+    // Check if ability can target the given creature
+    public bool CanTarget(Creature target, Creature caster)
+    {
+        if (target == null) return false;
+        
+        // Check if target is enemy or ally
+        bool isEnemy = true; // You'll need to implement team checking
+        bool isAlly = !isEnemy;
+        
+        switch (targetType)
+        {
+            case AbilityTargetType.Self:
+                return target == caster;
+            case AbilityTargetType.Ally:
+                return isAlly;
+            case AbilityTargetType.Enemy:
+                return isEnemy;
+            case AbilityTargetType.Any:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
+
+public enum AbilityTargetType
+{
+    Self,       // Can only target self
+    Ally,       // Can only target allies
+    Enemy,      // Can only target enemies
+    Any         // Can target anyone
+}
+
+public enum AbilityEffectType
+{
+    Damage,     // Deal damage
+    Heal,       // Restore HP
+    Defend,     // Increase defense
+    Buff,       // Apply positive effect
+    Debuff      // Apply negative effect
+}
+
